d036b344c9494b6e4ccdda181b2040ee0861cd36
maksim d <mak962753@gmail.com>
2025-01-08 00:49:42 +0300

    test#4

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,13 @@
 import {createRouter, createWebHistory} from "vue-router";
 
 const routes = [
+//   {
+//     path: "/",
+//     name: "home",
+//     component: () => import("../views/Home.vue"),
+//   },
   {
     path: "/",
-    name: "home",
-    component: () => import("../views/Home.vue"),
-  },
-  {
-    path: "/cube",
     name: "cube",
     component: () => import("../views/Cube.vue"),
   },
